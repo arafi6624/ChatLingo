f35e75de73b2b537b4ef6536a5aac64816d254ad
Abdul Rafi <abdulrafi@Abduls-MacBook-Pro.local>
2024-03-05 11:23:21 -0500

    Chat app frontend complete

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default function StartPage() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <ActivityIndicator size="large" color="gray"/>
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <ActivityIndicator size="large" color="gray" />
     </View>
   )
 }
